29ff278d291c2ede615d528b99fb9a6480baec7b
timoschwarzer <timoschwarzer@users.noreply.github.com>
2021-05-29 20:07:52 +0000

    deploy: b17ccf1ebf0d0f9858123e896f23180c32811407

---
+++ _nuxt/static/1622318870/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fori-rando-wiki\u002F_nuxt\u002Fstatic\u002F1622318870",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fori-rando-wiki\u002F",assetsPath:"\u002Fori-rando-wiki\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e577f6b4"}}}}(null));
