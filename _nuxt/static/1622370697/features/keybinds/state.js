3a464894dddd36fc66c46bb8098fbed22bdcb520
MunWolf <MunWolf@users.noreply.github.com>
2021-05-30 10:42:14 +0000

    deploy: 9c06beab46819e77543fd24cf5fe38c816488dd5

--- _nuxt/static/1622370697/features/keybinds/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622370697",layout:"default",error:a,serverRendered:true,routePath:"\u002Ffeatures\u002Fkeybinds",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7dae0ea4"}}}}(null));
