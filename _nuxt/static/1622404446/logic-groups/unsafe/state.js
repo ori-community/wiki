be11b32ac3f0da9c83815fbcffde27f7a0c54e27
timoschwarzer <timoschwarzer@users.noreply.github.com>
2021-05-30 19:55:06 +0000

    deploy: a8e60fe981fb0b1eee48b0c2323ac69126fa2b84

--- _nuxt/static/1622404446/logic-groups/unsafe/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622404446",layout:"default",error:a,serverRendered:true,routePath:"\u002Flogic-groups\u002Funsafe",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d74e8dd5"}}}}(null));
