8db8baf187aab1dc487555c5bc224feb7cea4a60
timoschwarzer <timoschwarzer@users.noreply.github.com>
2021-05-30 14:02:14 +0000

    deploy: 876a7192ea803cad1a24e9ff7b28918f4b096b8f

--- _nuxt/static/1622382524/logic-groups/gorlek/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622382524",layout:"default",error:a,serverRendered:true,routePath:"\u002Flogic-groups\u002Fgorlek",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f80533ec"}}}}(null));
