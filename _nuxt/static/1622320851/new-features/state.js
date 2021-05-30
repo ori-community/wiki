16a1a5a17e66c6cc02dea234e4cb9d1d594b325a
MunWolf <MunWolf@users.noreply.github.com>
2021-05-30 08:34:58 +0000

    deploy: 4441079641e56a20bf69d894fa92f8ad8e04ea5b

--- _nuxt/static/1622320851/new-features/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622320851",layout:"default",error:a,serverRendered:true,routePath:"\u002Fnew-features",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4ecf1f64"}}}}(null));
