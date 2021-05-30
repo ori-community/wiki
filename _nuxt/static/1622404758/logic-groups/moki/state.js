41775cabc8924eec35f0a72bedf1b72876d89d9c
timoschwarzer <timoschwarzer@users.noreply.github.com>
2021-05-30 19:59:40 +0000

    deploy: 21a6d94586cffce5f99e6933f4e2429921bd9bd7

--- _nuxt/static/1622404758/logic-groups/moki/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622404758",layout:"default",error:a,serverRendered:true,routePath:"\u002Flogic-groups\u002Fmoki",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9744eca1"}}}}(null));
