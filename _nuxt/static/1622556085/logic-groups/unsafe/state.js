8e0617e93322c55f76801a1365bb2a4751d68718
Foopyo <Foopyo@users.noreply.github.com>
2021-06-01 14:01:29 +0000

    deploy: b8e90ba7a297dea681c4b037a087eb6f293fe41b

---
+++ _nuxt/static/1622556085/logic-groups/unsafe/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622556085",layout:"default",error:a,serverRendered:true,routePath:"\u002Flogic-groups\u002Funsafe",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e3b80b91"}}}}(null));
