04f010e399fcdb2a6357378427ce39eb8af5f8f6
Foopyo <Foopyo@users.noreply.github.com>
2021-06-01 14:10:00 +0000

    deploy: 6362a14e48ee4e3b6b577406a36ea97807cb2ba0

---
+++ _nuxt/static/1622556598/tutorials/movement/drop-shot/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622556598",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftutorials\u002Fmovement\u002Fdrop-shot",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5d78414b"}}}}(null));
