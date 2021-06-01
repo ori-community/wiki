bbec725bb9ef227df6d29ff15a9d0e83a2e3b56b
Foopyo <Foopyo@users.noreply.github.com>
2021-06-01 13:08:05 +0000

    deploy: c1a0619418147b171910c8016a8b9c0af96cb6e6

---
+++ _nuxt/static/1622552882/logic-groups/gorlek/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622552882",layout:"default",error:a,serverRendered:true,routePath:"\u002Flogic-groups\u002Fgorlek",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c7d7e0ad"}}}}(null));
