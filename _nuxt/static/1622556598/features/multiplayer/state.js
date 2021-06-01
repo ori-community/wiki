c8adfe2c61a3b0c926528a376bc356cf4dc2787f
Foopyo <Foopyo@users.noreply.github.com>
2021-06-01 14:50:36 +0000

    deploy: 83c7b637d7e4c54c0f07fbf60ea4795463a6df69

--- _nuxt/static/1622556598/features/multiplayer/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622556598",layout:"default",error:a,serverRendered:true,routePath:"\u002Ffeatures\u002Fmultiplayer",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5d78414b"}}}}(null));
