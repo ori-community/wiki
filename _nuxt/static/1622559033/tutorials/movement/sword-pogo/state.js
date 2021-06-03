d9135399c3be8317734c5588fb0b959f24cfe5bb
Foopyo <Foopyo@users.noreply.github.com>
2021-06-03 14:08:50 +0000

    deploy: 1d8a6b1ba49220d3a5b2cd67614022721e522b7e

--- _nuxt/static/1622559033/tutorials/movement/sword-pogo/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622559033",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftutorials\u002Fmovement\u002Fsword-pogo",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"aa6ca9fd"}}}}(null));
