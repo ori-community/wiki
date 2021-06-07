1cc7c84348987a04fbf273bfba0d90ab48305ca5
Timo Schwarzer <timo@schwarzer.dev>
2021-06-07 21:18:29 +0200

    Only process .md files when generating static files

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,11 @@ export default {
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1'},
       {hid: 'description', name: 'description', content: 'Wiki for the Ori and the Will of the Wisps Randomizer'},
-      {hid: 'og:description', property: 'og:description', content: 'Wiki for the Ori and the Will of the Wisps Randomizer'},
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: 'Wiki for the Ori and the Will of the Wisps Randomizer'
+      },
       {hid: 'og:type', property: 'og:type', content: 'website'},
       {hid: 'og:url', property: 'og:url', content: 'https://ori-rando-wiki.github.io'},
       {hid: 'og:title', property: 'og:title', content: 'Ori Randomizer Wiki'},
@@ -35,27 +39,24 @@ export default {
 
   generate: {
     fallback: true,
-    async routes () {
-      const { $content } = require('@nuxt/content')
-      const files = await $content({ deep: true }).only(['path', 'slug']).fetch()
-      return mapContentPaths(files).map(article => article.path)
+    async routes() {
+      const {$content} = require('@nuxt/content')
+      const files = await $content({deep: true}).only(['path', 'slug', 'extension']).fetch()
+      return mapContentPaths(files.filter(f => f.extension === '.md')).map(article => article.path)
     }
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
@@ -76,6 +77,5 @@ export default {
   content: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {}
 }
