2bd0a854015eef5d93fa62f445816bb9ee0be373
timoschwarzer <timoschwarzer@users.noreply.github.com>
2021-06-27 18:44:18 +0000

    deploy: 8a65d84c19ce3695bbc01f6d82c559dcad136cbe

--- _nuxt/static/1624819322/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624819322",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ab79f630"}}}}(null,"\u002F"));
