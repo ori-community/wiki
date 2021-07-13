71b7df8310dbf5ab0924992b36180ab9ace76ee7
Foopyo <Foopyo@users.noreply.github.com>
2021-07-13 17:45:02 +0000

    deploy: c0d40e542316c1771b13601b6cfcfd0222e650ef

--- _nuxt/static/1626196154/logic-groups/glitched/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626196154",layout:"default",error:a,serverRendered:true,routePath:"\u002Flogic-groups\u002Fglitched",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6e41b1d7"}}}}(null));
