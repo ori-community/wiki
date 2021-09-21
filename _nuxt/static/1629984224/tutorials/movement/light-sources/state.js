047ba035103ffe2120a9d847562eb376cd91baf5
Foopyo <Foopyo@users.noreply.github.com>
2021-09-21 13:10:55 +0000

    deploy: 58586ef10560d34b267bc511d42ed56c8bb99679

--- _nuxt/static/1629984224/tutorials/movement/light-sources/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1629984224",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftutorials\u002Fmovement\u002Flight-sources",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"905217c9"}}}}(null));
