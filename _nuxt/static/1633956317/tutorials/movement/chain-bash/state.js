2d94b73d3f138825fd44c2653548ef7596b26177
timoschwarzer <timoschwarzer@users.noreply.github.com>
2021-10-11 13:21:07 +0000

    deploy: 27df3c4f6d2439187426f9d4805a7ee3f2ee869e

--- _nuxt/static/1633956317/tutorials/movement/chain-bash/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1633956317",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftutorials\u002Fmovement\u002Fchain-bash",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"364db03e"}}}}(null));
