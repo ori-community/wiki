4f4e4ce3363be28b70cc63202e5a4bfe6e0f84d5
Foopyo <Foopyo@users.noreply.github.com>
2021-10-23 15:58:25 +0000

    deploy: 91a2b6097bf600e232742ee3c67cb2a1269a9560

--- _nuxt/static/1635004449/seedgen/paths/moki/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635004449",layout:"default",error:a,serverRendered:true,routePath:"\u002Fseedgen\u002Fpaths\u002Fmoki",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f740a584"}}}}(null));
