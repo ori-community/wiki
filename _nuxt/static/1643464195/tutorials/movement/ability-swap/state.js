1446f1d53b3cc2fc0a723c8562cd6ae59d4d5131
Foopyo <Foopyo@users.noreply.github.com>
2022-02-08 11:31:13 +0000

    deploy: 51936c284c47805635deac5af1ffd50570682f53

--- _nuxt/static/1643464195/tutorials/movement/ability-swap/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643464195",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftutorials\u002Fmovement\u002Fability-swap",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3e09c64c"}}}}(null));
