763c655c42f9a33c6ec63cb32db45f6a766a87a6
Foopyo <Foopyo@users.noreply.github.com>
2022-02-08 15:59:52 +0000

    deploy: 63964b0423e4f2051c5a4ccf2640cd69dd5b8ef9

---
+++ _nuxt/static/1644335987/seedgen/presets/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1644335987",layout:"default",error:a,serverRendered:true,routePath:"\u002Fseedgen\u002Fpresets",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"84356751"}}}}(null));
