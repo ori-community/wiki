514ca375b0eb4096b9aa9bfba2011c8805a0ddb7
Foopyo <Foopyo@users.noreply.github.com>
2022-02-28 11:53:26 +0000

    deploy: eaff1604b306b2947dbdcae35674421173abe64b

--- _nuxt/static/1646049016/seedgen/generator/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1646049016",layout:"default",error:a,serverRendered:true,routePath:"\u002Fseedgen\u002Fgenerator",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"102510fa"}}}}(null));
--- _nuxt/static/1646049016/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1646049016",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"102510fa"}}}}(null,"\u002F"));
