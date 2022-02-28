a26d66d47f17ebd1cb7adac9dbbcdc0f50bae364
Foopyo <Foopyo@users.noreply.github.com>
2022-02-28 11:50:21 +0000

    deploy: 6932eec5d28a8f29e5d2f4db8494847fbec9a259

---
+++ _nuxt/static/1646049016/tutorials/sequence-breaks/dash-swim/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1646049016",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftutorials\u002Fsequence-breaks\u002Fdash-swim",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"102510fa"}}}}(null));
