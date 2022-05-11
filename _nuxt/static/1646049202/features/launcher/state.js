3d53f2b551097efcafb5fbc69671b65a34cd98dd
Foopyo <Foopyo@users.noreply.github.com>
2022-05-11 12:21:50 +0000

    deploy: f58d9fa7047dd2c17290c8378257898e2da669a5

--- _nuxt/static/1646049202/features/launcher/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1646049202",layout:"default",error:a,serverRendered:true,routePath:"\u002Ffeatures\u002Flauncher",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5a4a0de9"}}}}(null));
---
+++ _nuxt/static/1652271706/features/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1652271706",layout:"default",error:a,serverRendered:true,routePath:"\u002Ffeatures",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c63d2212"}}}}(null));
