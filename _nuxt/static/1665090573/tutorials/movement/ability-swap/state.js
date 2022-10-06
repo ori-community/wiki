e5b843560b248aa0d0dffa7828300dfc294d950a
42Kil <42Kil@users.noreply.github.com>
2022-10-06 22:10:34 +0000

    deploy: 48f64095929fb3acd12e0efab65cb1be942d8189

--- _nuxt/static/1665090573/tutorials/movement/ability-swap/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1665090573",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftutorials\u002Fmovement\u002Fability-swap",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b38ef579"}}}}(null));
