83e6ca79684dd40a77c48306b3d6ce1f60e9f929
42Kil <42Kil@users.noreply.github.com>
2022-10-06 21:09:38 +0000

    deploy: 6be4affd7f817483e130bc047da0b7fed421f2f9

---
+++ _nuxt/static/1665090573/features/multiplayer/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1665090573",layout:"default",error:a,serverRendered:true,routePath:"\u002Ffeatures\u002Fmultiplayer",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b38ef579"}}}}(null));
