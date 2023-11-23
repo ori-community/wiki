266a716bb2f1ae4c712b9239c65d3d70b33cfe1f
timoschwarzer <timoschwarzer@users.noreply.github.com>
2023-11-23 12:40:20 +0000

    deploy: da4b3d946784eb3e1851c8c87e7a1f4ec7eef880

--- _nuxt/static/1678273021/tutorials/misc/wall-break/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1678273021",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftutorials\u002Fmisc\u002Fwall-break",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bad8c219"}}}}(null));
