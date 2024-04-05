c3831f6e5f6c498160d6caf3d8b315641ff7ca05
Mawex <Mawex@users.noreply.github.com>
2024-04-05 11:57:39 +0000

    deploy: e298a39b69a960ad9ff85bb7c6b81003e4cccaad

---
+++ _nuxt/static/1712318255/tutorials/movement/portal-hyperspeed/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1712318255",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftutorials\u002Fmovement\u002Fportal-hyperspeed",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1b74c954"}}}}(null));
