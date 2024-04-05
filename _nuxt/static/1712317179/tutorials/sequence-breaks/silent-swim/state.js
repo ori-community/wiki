4d41afff16bb4ff0767d6f0401dc4daed7454c09
Mawex <Mawex@users.noreply.github.com>
2024-04-05 11:39:42 +0000

    deploy: ac4d3d58b9a089f19cb673c7c5ab84961134ba2b

---
+++ _nuxt/static/1712317179/tutorials/sequence-breaks/silent-swim/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1712317179",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftutorials\u002Fsequence-breaks\u002Fsilent-swim",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"54523668"}}}}(null));
