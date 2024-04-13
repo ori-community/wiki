854afdb22064ed9be00225a335c37277ec4ebc33
Mawex <Mawex@users.noreply.github.com>
2024-04-13 23:07:29 +0000

    deploy: 5ad0e7377f8f76e72e89037f5c39939069c25a34

--- _nuxt/static/1713039065/seedgen/paths/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1713039065",layout:"default",error:a,serverRendered:true,routePath:"\u002Fseedgen\u002Fpaths",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ba917ef4"}}}}(null));
---
+++ _nuxt/static/1713049645/tutorials/movement/wavedash/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1713049645",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftutorials\u002Fmovement\u002Fwavedash",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"3f4c48bf"}}}}(null));
