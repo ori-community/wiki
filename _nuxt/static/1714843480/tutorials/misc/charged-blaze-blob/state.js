12d29a4d46058f6763986ba5f7e0d35f98e78237
timoschwarzer <timoschwarzer@users.noreply.github.com>
2024-05-04 17:24:43 +0000

    deploy: f88315f2cc9f550b7c3213432d69322f2fb1009f

---
+++ _nuxt/static/1714843480/tutorials/misc/charged-blaze-blob/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1714843480",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftutorials\u002Fmisc\u002Fcharged-blaze-blob",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bbc069ed"}}}}(null));
