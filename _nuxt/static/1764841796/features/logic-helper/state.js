3b4ecf64479dbe7a7a16e52a035153cd82a42d24
timoschwarzer <timoschwarzer@users.noreply.github.com>
2025-12-31 10:51:56 +0000

    deploy: 13d955305fa50858eb6a8a13967436f00aa16364

--- _nuxt/static/1764841796/features/logic-helper/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1764841796",layout:"default",error:a,serverRendered:true,routePath:"\u002Ffeatures\u002Flogic-helper",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"12a2b9a1"}}}}(null));
