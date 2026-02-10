134bfc5197a6466c998581ea71772e0f7b76d34b
timoschwarzer <timoschwarzer@users.noreply.github.com>
2026-02-10 09:04:38 +0000

    deploy: 2be06385ddac09e4cc974489fe7a338d1af39a2b

--- _nuxt/static/1767178312/tutorials/movement/dash-ramp/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1767178312",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftutorials\u002Fmovement\u002Fdash-ramp",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2913f29d"}}}}(null));
