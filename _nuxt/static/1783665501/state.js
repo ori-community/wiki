0d3ee2c75f02df280229750b7b0c7e24f5a7c1b8
Foopyo <Foopyo@users.noreply.github.com>
2026-07-10 06:38:24 +0000

    deploy: e00a83a788a71efc9416088b42300fe0506c8386

---
+++ _nuxt/static/1783665501/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1783665501",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"69f49f8d"}}}}(null,"\u002F"));
